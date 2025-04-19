b3e21fda09e1565d6d397b65302ff7d13e6efa51
prasanna <prasannakiran229@gmail.com>
2025-04-19 21:36:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum XOR of two numbers in an array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.maxXor(arr));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution
+{
+    static class TrieNode 
+    {
+        TrieNode[] children = new TrieNode[2];
+    }
+    public int maxXor(int[] arr) 
+    {
+        TrieNode root = new TrieNode();
+        int maxXor = 0;
+        insert(root, arr[0]);
+        for (int i = 1; i < arr.length; i++) 
+        {
+            maxXor = Math.max(maxXor, findMaxXor(root, arr[i]));
+            insert(root, arr[i]);
+        }
+        return maxXor;
+    }
+    private void insert(TrieNode root, int num) 
+    {
+        TrieNode node = root;
+        for (int i = 31; i >= 0; i--) 
+        {
+            int bit = (num >> i) & 1;
+            if (node.children[bit] == null) 
+            {
+                node.children[bit] = new TrieNode();
+            }
+            node = node.children[bit];
+        }
+    }
+    private int findMaxXor(TrieNode root, int num) 
+    {
+        TrieNode node = root;
+        int maxXor = 0;
+        for (int i = 31; i >= 0; i--) 
+        {
+            int bit = (num >> i) & 1;
+            int toggledBit = 1 - bit;
+            if (node.children[toggledBit] != null) 
+            {
+                maxXor |= (1 << i);
+                node = node.children[toggledBit];
+            } else 
+            {
+                node = node.children[bit];
+            }
+        }
+        return maxXor;
+    }
+}
