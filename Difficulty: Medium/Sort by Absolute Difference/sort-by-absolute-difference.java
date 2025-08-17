48121af32fed2db01bdaf30cafc60280711631e1
prasanna <prasannakiran229@gmail.com>
2025-08-17 21:36:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort by Absolute Difference/sort-by-absolute-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rearrange(int[] arr, int x) {
+        Integer[] temp = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(temp, (a, b) -> {
+            int diff1 = Math.abs(a - x);
+            int diff2 = Math.abs(b - x);
+            return diff1 - diff2;
+        });
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+    }
+}
