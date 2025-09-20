57c62195482b40d345a47a5d51f8ee1494f8f131
prasanna <prasannakiran229@gmail.com>
2025-09-20 23:13:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.java
@@ -0,0 +1,35 @@
+class Solution {
+    public double medianOf2(int a[], int b[]) {
+        if (a.length > b.length) {
+            return medianOf2(b, a); // binary search on smaller array
+        }
+
+        int n = a.length;
+        int m = b.length;
+        int low = 0, high = n;
+
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (n + m + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : a[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : b[cut2 - 1];
+            int r1 = (cut1 == n) ? Integer.MAX_VALUE : a[cut1];
+            int r2 = (cut2 == m) ? Integer.MAX_VALUE : b[cut2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+        }
+
+        return 0.0; // should not reach here
+    }
+}
