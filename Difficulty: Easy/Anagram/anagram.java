d83b6a77ae44d7b5fcac528c339d5dc4f22dc0c1
prasanna <prasannakiran229@gmail.com>
2026-09-14 17:07:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        int freq[]=new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq[s1.charAt(i)-'a']++;
+            freq[s2.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+        // code here
+        
+    }
+}
